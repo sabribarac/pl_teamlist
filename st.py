782e9486c0d1bc6e3465d06d9747228283865ca3
Sabri Barac <sabribarac@gmail.com>
2023-08-26 17:40:34 +0200

    Update st.py

--- st.py
+++ st.py
@@ -6,12 +6,18 @@ def main():
     st.title("Premier League Table")
 
     url = "https://www.bbc.com/sport/football/premier-league/table"
-    html_content = requests.get(url).text
-    soup = BeautifulSoup(html_content, 'html.parser')
-    team_name_elements = soup.find_all('a', class_='ssrcss-13tfrt4-TeamName e1uquauq3')
-    team_names = [element.get_text() for element in team_name_elements]
+    response = requests.get(url)
 
-    team_table = "\n".join([f"{idx}. {team_name}" for idx, team_name in enumerate(team_names, start=1)])
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        team_name_elements = soup.find_all("span", class_="long")
+        team_names = [element.get_text(strip=True) for element in team_name_elements[:20]]
+        
+        team_table = "\n".join([f"{idx}. {team_name}" for idx, team_name in enumerate(team_names, start=1)])
+    else:
+        print("Failed to fetch the webpage.")
+
+    
     
     team_list = st.text_area("Team List", team_table, height=500)
 
